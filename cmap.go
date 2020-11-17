b901cceb005d6785363947a839d4022756ebd1e8
Rutvik <rutvik20007@gmail.com>
2020-11-17 17:35:02 -0500

    Added some boilerplate stuff

--- cmap.go
+++ cmap.go
@@ -1,25 +1,53 @@
-
 package main
+
 import "fmt"
 
-//type struct {
-//
-//}
-
-func NewChannelMap() {
-
-
-	return chanMap;
+//ChannelMap hola
+type ChannelMap struct {
+	intChan chan int
 }
 
-func NewLockingMap() {
+// Listen hola
+func (cm ChannelMap) Listen() {
+	return
+}
 
+//Stop hola
+func (cm ChannelMap) Stop() {
+	return
+}
 
-	return nil;
+// Reduce hola
+func (cm ChannelMap) Reduce(functor ReduceFunc, accumStr string, accumInt int) (string, int) {
+
+	return "Hello", 1
+}
+
+// AddWord hola
+func (cm ChannelMap) AddWord(word string) {
+
+	return
+
+}
+
+// GetCount hola
+func (cm ChannelMap) GetCount(word string) int {
+	return 1
+}
+
+//NewChannelMap returns a new channelMap
+func NewChannelMap() EmergingMap {
+	var tmpChanMap ChannelMap
+	tmpChanMap.intChan = make(chan int)
+	return tmpChanMap
+}
+
+//NewLockingMap returns a new ChannelMap
+func NewLockingMap() EmergingMap {
+
+	return NewChannelMap()
 }
 
 func main() {
 	fmt.Printf("hello world\n")
 }
-
-
